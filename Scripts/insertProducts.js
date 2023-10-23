31e8aef9a78167fb6c5a8496c06bdb5ec4bbdaa2
Nevec02 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2023-10-23 11:51:04 +0200

    extras 2

--- Scripts/insertProducts.js
+++ Scripts/insertProducts.js
@@ -28,7 +28,7 @@ export const insertProducts = () => {
       // Limpiar la tabla antes de la nueva insercción
       const cleanTbody = document.getElementById("clean-table");
 
-      cleanTbody.innetHTML = "";
+      cleanTbody.innertHTML = "";
       // Limpiar los campos del formulario
       productForm.reset();
 
